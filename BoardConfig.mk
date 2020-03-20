21d9fdc5de607021a238b62d81febd468a0622f8
BitO BSessiOn <bitobsession@gmail.com>
2020-03-20 13:12:42 +0100

    Update for firmware 9.0.11
    Other minor updates

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,14 +34,6 @@ TARGET_OTA_ASSERT_DEVICE := cheeseburger,OnePlus5,oneplus5
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
-# Lineage Hardware
-#BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/lineagehw
-
-# SELinux
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-
 # inherit from the proprietary version
 -include vendor/oneplus/cheeseburger/BoardConfigVendor.mk
 
