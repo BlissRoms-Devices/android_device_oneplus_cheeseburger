3c151e21fa637aeb25ad26449b39d896eb38b24a
BitO BSessiOn <bitobsession@gmail.com>
2020-11-10 16:23:12 +0100

    Blissify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_cheeseburger.mk
+    $(LOCAL_DIR)/bliss_cheeseburger.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_cheeseburger-user \
-    lineage_cheeseburger-userdebug \
-    lineage_cheeseburger-eng
+    bliss_cheeseburger-user \
+    bliss_cheeseburger-userdebug \
+    bliss_cheeseburger-eng
